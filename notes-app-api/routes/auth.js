3783b4cb8f9196c8dc57c40920ac2bded668e581
unknown <ishimwejennymiriotta@gmail.com>
2026-04-25 23:56:50 +0200

    addition of User model for authentication system

---
+++ notes-app-api/routes/auth.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+router.post('/register',async(req,res)=>{
+    const 
+})
