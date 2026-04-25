2e0ca01a973e06bc74dd1f66532d9fe1992f310d
unknown <ishimwejennymiriotta@gmail.com>
2026-04-26 00:04:08 +0200

    addition of Note schema with user reference

---
+++ notes-app-api/models/notes.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+const NotesSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    content:{
+        type:String,
+        required:true
+    },
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User'
+    }
+})
+module.exports = mongoose.model('Note',NoteSchema)
