bbdcf3ce829e26c84b2ef04a488bc49ae08c3b2a
unknown <ishimwejennymiriotta@gmail.com>
2026-04-26 11:03:08 +0200

    implemention of register and login with JWT in notes_app_api

--- notes-app-api/routes/auth.js
+++ notes-app-api/routes/auth.js
@@ -5,5 +5,33 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 router.post('/register',async(req,res)=>{
-    const 
+    const {name,email,password} = req.body
+    if(await findOne({email})){
+        return res.status(400).send('User already exists')
+    }
+    const hashedPassword = await bcrypt.hash(password,10)
+    const newUser = new User({name,email,password:hashedPassword})
+    await newUser.save()
+    res.status(201).send('User registered successfully')
+})  
+
+router.post('./login',async(req,res) => {
+    const {name,email,password} = req.body
+    const isUser = await User.findOne({email})
+    if(!isUser){
+        return res.status(400).send('User not found')
+    }else{
+        const isMatch = await bcrypt.compare(password,isUser.password)
+        if(!isMatch){
+            return res.status(400).send('Incorrect password!')
+        }
+        const token = jwt.sign(
+            {id:isUser._id},
+            process.env.JWT_SECRET,
+            {expiresIn:'1d'}
+        )
+        res.send({token})
+    }
 })
+
+module.exports = router
