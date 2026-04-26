55fafef14cff993b2b4ff761bb32d4c6b8477caf
unknown <ishimwejennymiriotta@gmail.com>
2026-04-26 11:35:39 +0200

    resolving issues in notes routes in notes_app_api

--- notes-app-api/routes/notes.js
+++ notes-app-api/routes/notes.js
@@ -1,27 +1,27 @@
-const express = reuqire('express')
+const express = require('express')
 const router = express.Router()
 const auth = require('../middleware/auth')
-const notes = require('../models/notes')
+const Note = require('../models/notes')
 
 router.get('/',auth, async(req,res)=>{
-    const Notes = await notes.find({userId:req.user.id})
-    res.json(myTasks)
+    const myNotes = await Note.find({userId:req.user.id})
+    res.json(myNotes)
 })
 
 router.post('/',auth,async(req,res)=>{
     const {title,content} = req.body
-    const newNote = new note({title,content,userId:req.user.id})
+    const newNote = new Note({title,content,userId:req.user.id})
     await newNote.save()
     res.json(newNote) 
 })
 
 router.put('/:id',auth,async(req,res)=>{
     const id = req.params.id
-    const note = await note.findById(id)
+    const note = await Note.findById(id)
     if(!note){
         return res.status(404).send('Note not found')
     } 
-    const isMatch = note.userId.toString() === re.user.id.toString()
+    const isMatch = note.userId.toString() === req.user.id.toString()
     if(!isMatch){
         return res.status(403).send("Not allowed")
     }
@@ -32,15 +32,15 @@ router.put('/:id',auth,async(req,res)=>{
     res.json(note) 
 })
 router.delete('/:id',auth,async(req,res)=>{
-    const note = await note.findById(re.params.id)
+    const note = await Note.findById(req.params.id)
     if(!note){
         return res.status(404).send('Note not found')
     }
-    const isMatch = note.userId.toString() === req.params.id.toString()
+    const isMatch = note.userId.toString() === req.user.id.toString()
     if(!isMatch){
         return res.status(403).send("Not allowed")
     }
-    await note.findByIdAndDelete(req.params.id)
+    await Note.findByIdAndDelete(req.params.id)
     res.send('Note deleted successfully')
 })
 module.exports = router
