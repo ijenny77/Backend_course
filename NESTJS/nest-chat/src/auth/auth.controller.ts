bdf792e3c53a12259919f7ac20aae982e2f9728f
unknown <ishimwejennymiriotta@gmail.com>
2026-07-17 00:58:24 +0200

    Implement login endpoint with JWT issuance

--- NESTJS/nest-chat/src/auth/auth.controller.ts
+++ NESTJS/nest-chat/src/auth/auth.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller,Post, UnauthorizedException,Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService:AuthService){}
+
+    @Post('login')
+    async login(@Body() loginDto:LoginDto){
+        const user = await this.authService.validateUser(loginDto.username,loginDto.password)
+        if(!user){
+            throw new UnauthorizedException('Invalid credentials')
+        }
+        return this.authService.login(user)
+    }
+
+}
--- NESTJS/nest-chat/src/auth/auth.service.ts
+++ NESTJS/nest-chat/src/auth/auth.service.ts
@@ -8,4 +8,19 @@ export class AuthService {
         private usersService:UsersService,
         private jwtService:JwtService
     ){}
+    async validateUser(username:string,password:string){
+        const user = await this.usersService.findByUsername(username)
+        if(!user) return null;
+
+        const passwordMatches = await bcrypt.compare(password,user.password)
+        if(!passwordMatches) return null;
+
+        return user;
+    }
+    async login(user : { id:string; username:string }){
+        const payload = { sub: user.id , username:user.username }
+        return{
+            access_token : this.jwtService.sign(payload)
+        }
+    }
 }
