64dfca90cc98e6a24e68fe0aaa48a79ad61b4e29
unknown <ishimwejennymiriotta@gmail.com>
2026-07-23 23:55:39 +0200

    Add CreateMessageDto and scaffold MessagesService repository injection

---
+++ NESTJS/nest-chat/src/messages/dto/create-message.dto.ts
@@ -0,0 +1,10 @@
+import { IsNotEmpty,IsString } from "class-validator";
+
+export class CreateMessageDto{
+    @IsString()
+    @IsNotEmpty()
+    content:string;
+    @IsString()
+    @IsNotEmpty()
+    roomId:string;
+}
